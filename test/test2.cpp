65f30968efd8502cb5a6ed365dd8cf88ab23cd3a
Ian Bell <ian.bell@nist.gov>
2019-05-31 17:04:51 -0600

    Print NBP in serial

--- test/test2.cpp
+++ test/test2.cpp
@@ -20,10 +20,13 @@ int main() {
         handles.push_back(handle);
     }
 
-    // Now we do a serial evaluation, calculation of the NBP temperature for each one
-    //int kq = 1;
-    //double z[] = { 1.0 }, x[] = { 1.0 }, y[] = { 1.0 }, T = 300, p = 101.325, d = -1, dl = -1, dv = -1, h = -1, s = -1, u = -1, cp = -1, cv = -1, q = 0, w = -1;
-    //PQFLSHdll(handle, &handle_errcode, p, q, z, kq, T, d, dl, dv, x, y, u, h, s, cp, cv, w, ierr, herr, 255);
+    for (auto handle: handles){
+        // Now we do a serial evaluation, calculation of the NBP temperature for each one
+        int kq = 1;
+        double z[20] = { 1.0 }, x[20] = { 1.0 }, y[20] = { 1.0 }, T = -100, p = 101.325, d = -1, dl = -1, dv = -1, h = -1, s = -1, u = -1, cp = -1, cv = -1, q = 0, w = -1;
+        PQFLSHdll(handle, &handle_errcode, p, q, z, kq, T, d, dl, dv, x, y, u, h, s, cp, cv, w, ierr, herr, 255);
+        std::cout << T << std::endl;
+    }
 
     return EXIT_SUCCESS;
 }
