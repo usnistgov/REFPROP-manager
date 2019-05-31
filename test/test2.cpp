9a6d187dd5b27c06701be1745d24f2ab99fd179e
Ian Bell <ian.bell@nist.gov>
2019-05-31 17:06:08 -0600

    Check good load

--- test/test2.cpp
+++ test/test2.cpp
@@ -12,6 +12,10 @@ int main() {
         char errmsg[255];
         int handle_errcode = 0;
         int handle = construct_handle("D:/Program Files (x86)/REFPROP/", "REFPRP64.dll", &handle_errcode, errmsg, 255);
+        if (handle_errcode != 0){
+            std::cout << "Could not load REFPROP; fail!\n";
+            return EXIT_FAILURE;
+        }
         int ierr = 0;
         char hfld[10001] = "";
         std::string name = names[1].c_str();
@@ -23,6 +27,7 @@ int main() {
     for (auto handle: handles){
         // Now we do a serial evaluation, calculation of the NBP temperature for each one
         int kq = 1;
+        int ierr = 0;
         double z[20] = { 1.0 }, x[20] = { 1.0 }, y[20] = { 1.0 }, T = -100, p = 101.325, d = -1, dl = -1, dv = -1, h = -1, s = -1, u = -1, cp = -1, cv = -1, q = 0, w = -1;
         PQFLSHdll(handle, &handle_errcode, p, q, z, kq, T, d, dl, dv, x, y, u, h, s, cp, cv, w, ierr, herr, 255);
         std::cout << T << std::endl;
