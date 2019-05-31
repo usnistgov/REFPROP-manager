483ad8e9dd1332788a8676acdbf6605d266eaa2c
Ian Bell <ian.bell@nist.gov>
2019-05-31 16:57:29 -0600

    No handle

--- test/test2.cpp
+++ test/test2.cpp
@@ -14,14 +14,14 @@ int main() {
         int ierr = 0;
         char hfld[255] = std::string(255, " ").c_str();
         strcpy(hfld, names[1].c_str());
-        handle.SETFLUIDSdll(handle, &handle_errcode, hfld, ierr);
+        SETFLUIDSdll(handle, &handle_errcode, hfld, ierr);
         handles.push_back(handle);
     }
 
     // Now we do a serial evaluation, calculation of the NBP temperature for each one
-    int kq = 1;
-    double z[] = { 1.0 }, x[] = { 1.0 }, y[] = { 1.0 }, T = 300, p = 101.325, d = -1, dl = -1, dv = -1, h = -1, s = -1, u = -1, cp = -1, cv = -1, q = 0, w = -1;
-    PQFLSHdll(handle, &handle_errcode, p, q, z, kq, T, d, dl, dv, x, y, u, h, s, cp, cv, w, ierr, herr, 255);
+    //int kq = 1;
+    //double z[] = { 1.0 }, x[] = { 1.0 }, y[] = { 1.0 }, T = 300, p = 101.325, d = -1, dl = -1, dv = -1, h = -1, s = -1, u = -1, cp = -1, cv = -1, q = 0, w = -1;
+    //PQFLSHdll(handle, &handle_errcode, p, q, z, kq, T, d, dl, dv, x, y, u, h, s, cp, cv, w, ierr, herr, 255);
 
     return EXIT_SUCCESS;
 }
